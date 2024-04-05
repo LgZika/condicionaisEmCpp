88fd2b2a93b380915ee3defce11191d5eb51e9c9
Luiz Gustavo Dias Pulz <162385594+LgZika@users.noreply.github.com>
2024-04-05 00:39:32 +0000

    ex4

--- exercicios.cpp
+++ exercicios.cpp
@@ -6,18 +6,36 @@ int main(){
     int num;
 
     // Exercício 1
-    cout << "Entre com um numero: ";
+    cout << "1 - Entre com um numero: ";
     cin >> num;
     if (num > 10) {
         cout << "O numero eh maior que 10" << endl;
     }
 
     // Exercício 2
-    cout << "Entre com um numero: ";
+    cout << "2 - Entre com um numero: ";
     cin >> num;
     if (num == 0) {
         cout << "O numero digitado eh 0" << endl;
     }
 
+    // Exercício 3
+    cout << "3 - Entre com um numero: ";
+    cin >> num;
+    if (num < 0) {
+        cout << "O numero digitado eh negativo" << endl;
+    } else {
+        cout << "O numero digitado eh positivo ou zero" << endl;
+    }
+
+    // Exercício 4
+    cout << "4 - Entre com um numero: ";
+    cin >> num;
+    if (num % 2 == 0) {
+        cout << "O numero eh par" << endl;
+    } else {
+        cout << "O numero eh impar" << endl;
+    }
+    
     return false;
 }
